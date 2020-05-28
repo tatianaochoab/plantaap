15a7b166e06e8591ce22f7a5bfff5e9dd1bd1095
tatianaochoab <tata.8abautista@gmail.com>
2020-05-28 09:21:29 -0500

    Updates

---
+++ precache-manifest.0116e782b9e6980f6e64a770901dc107.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e7236255d43b235c1139b34d7debea",
+    "url": "/plantaap/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "249792c519b12cfc85fa",
+    "url": "/plantaap/static/css/main.0a54a213.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaap/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantaap/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249792c519b12cfc85fa",
+    "url": "/plantaap/static/js/main.fa7d0099.chunk.js"
+  },
+  {
+    "revision": "496bf7a6b1d56f31db05",
+    "url": "/plantaap/static/js/runtime-main.2e63ff50.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantaap/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantaap/static/media/logo.0d3d7212.png"
+  }
+]);
